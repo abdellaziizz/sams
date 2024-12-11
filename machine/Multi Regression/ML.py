bb8296f9c130911ed871e4bacf08949ebdb4e844
abdellaziizz <139694664+abdellaziizz@users.noreply.github.com>
2024-12-11 22:35:33 +0200

    Delete machine directory

--- machine/Multi Regression/ML.py
+++
@@ -1,9 +0,0 @@
-import pandas
-from sklearn import linear_model
-df = pandas.read_csv("cardekho_data.csv")
-X = df[['Selling_Price', 'Present_Price']]
-y = df['Kms_Driven']
-regr = linear_model.LinearRegression()
-regr.fit(X, y)
-predictedKMS = regr.predict([[27000, 55000]])
-print(predictedKMS)
